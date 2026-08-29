51cf207c53a723944575f42596626973a569892f
Shaheer Ahmed <mrshaheer75@gmail.com>
2026-08-29 17:27:54 +0500

    fix: reject negative scores

--- src/gradebook/gradebook.js
+++ src/gradebook/gradebook.js
@@ -4,6 +4,12 @@ class Student {
         this.studentId = rollNo;
         this.scores = [];
     }
+
+    addScore(){
+        if(this.scores < 0){
+            return "Negative number cannot be accepted"
+        }
+    }
 }
 
 module.exports = Student;

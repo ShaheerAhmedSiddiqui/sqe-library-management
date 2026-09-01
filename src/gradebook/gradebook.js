7fffc2f1d5332323227e33811a8fb5bed4f89e32
ShaheerAhmedSiddiqui <mrshaheer75@gmail.com>
2026-09-02 01:22:27 +0500

    Fixed Issue Of Rejecting score 100

--- src/gradebook/gradebook.js
+++ src/gradebook/gradebook.js
@@ -10,7 +10,7 @@ class Student {
             throw new TypeError("Score must be a number");
         }
 
-        if (score < 0 || score > 99) {
+        if (score < 0 || score > 100) {
             throw new RangeError("Score must be between 0 and 100");
         }
 
